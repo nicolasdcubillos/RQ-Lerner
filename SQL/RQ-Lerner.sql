02f99f548f062d4c67d7bae042247dc7fe780a43
Nicolas Cubillos <nicolasdavidcubillos@gmail.com>
2024-06-10 03:32:43 -0500

    Implementing Excel uploading logic (parsing as well).

---
+++ SQL/RQ-Lerner.sql
@@ -0,0 +1,32 @@
+USE AJOVECO_NE;
+
+DROP TABLE RQ_EXCEL_CONFIG;
+
+CREATE TABLE RQ_EXCEL_CONFIG (
+    COLUMN_NAME NVARCHAR(100),
+    EXCLUDE_VALIDATIONS BIT,
+    DATA_TYPE NVARCHAR(50),
+    POSITION INT
+);
+
+INSERT INTO RQ_EXCEL_CONFIG (COLUMN_NAME, EXCLUDE_VALIDATIONS, DATA_TYPE, POSITION) VALUES
+('cod proveedor', 0, 'I', 1),
+('nombre proveedor', 0, 'C', 2),
+('isbn', 0, 'C', 3),
+('titulo', 0, 'C', 4),
+('autor', 0, 'C', 5),
+('editorial', 0, 'C', 6),
+('tema', 0, 'C', 7),
+('precio', 0, 'N', 8),
+('cantidad', 0, 'I', 9),
+('SEDE', 0, 'C', 10),
+('LIBRERO', 0, 'C', 11),
+('ESTADO', 0, 'C', 12);
+
+SELECT * FROM RQ_EXCEL_CONFIG ORDER BY POSITION;
+
+SELECT * FROM CONSECUT WHERE TIPODCTO = 'RQ';
+
+INSERT INTO TRADE (ORIGEN, TIPODCTO, NRODCTO) VALUES ('COM', 'RQ', '1');
+
+SELECT * FROM TRADE;
