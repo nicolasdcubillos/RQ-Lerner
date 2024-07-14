628be11c2efd6a1b4dbb5188c108f16cfc5e85d5
Nicolas Cubillos <nicolasdavidcubillos@gmail.com>
2024-07-13 21:33:39 -0500

    Improving RQ-Uploads library and frontend.

--- SQL/RQ-Lerner.sql
+++ SQL/RQ-Lerner.sql
@@ -29,6 +29,8 @@ SELECT * FROM RQ_EXCEL_CONFIG ORDER BY POSITION;
 
 DROP PROCEDURE dbo.GuardarRequisicion;
 GO
+DROP PROCEDURE dbo.RQ_SaldoInventarioProducto;
+GO
 DROP FUNCTION [dbo].[RQ_ConsolidadoRequisiciones]
 GO
 
@@ -139,25 +141,140 @@ Return
 	MVTRADE.NOMBRE,
 	TRADE.CODCC,
 	TRADE.NIT AS RESPONSABLE,
-	MVTRADE.CANTIDAD
+	CAST(MVTRADE.CANTIDAD AS INTEGER) AS CANTIDAD,
+	CAST(MVTRADE.CANTIDAD AS INTEGER) AS CANTIDAD_FINAL
 	FROM
 	TRADE,
 	MVTRADE,
-	TIPODCTO,
-	FNVOF_REPORTECATALOGO(YEAR(@fecha2), MONTH(@fecha2)) AS RCATALOGO
+	TIPODCTO
 	WHERE
 	TIPODCTO.DCTOMAE = 'RQ' AND
 	TIPODCTO.TIPODCTO = 'RQ' AND
 	TRADE.NRODCTO = MVTRADE.NRODCTO AND
 	TRADE.TIPODCTO = MVTRADE.TIPODCTO AND
 	TRADE.ORIGEN = MVTRADE.ORIGEN AND
-	RCATALOGO.PRODUCTO = MVTRADE.PRODUCTO AND
-	TRADE.FECING BETWEEN @fecha1 AND @fecha2
+	TRADE.FECING BETWEEN @fecha1 AND DATEADD(DAY, 1, @fecha2)
 )
 
-/* SELECT * FROM RQ_ConsolidadoRequisiciones('20240615', '20240617') 
+/* 
+
+	EXEC dbo.GuardarRequisicion '99', '123', '123', '001                                               ', 'SPV739596           ', '5', '12000'
+	SELECT * FROM RQ_ConsolidadoRequisiciones('20240615', '20240626') 
+*/
+
+GO
+/*
+CREATE TABLE TEMP_UBICACIONES (
+    codigo VARCHAR(50) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    grupo VARCHAR(50) NOT NULL,
+    sigla VARCHAR(50) NOT NULL,
+    centrocosto VARCHAR(50) NOT NULL
+);
+
+SELECT 
+SIGLA 
+FROM 
+TEMP_UBICACIONES 
+WHERE
+SIGLA NOT LIKE '%APLICA%'
+GROUP BY 
+SIGLA;
+
+SELECT SIGLA FROM TEMP_UBICACIONES WHERE CODIGO = 'CA1'
 
 
-EXEC dbo.GuardarRequisicion '99', '123', '123', '001                                               ', 'SPV739596           ', '5', '12000'
+SELECT GRUPO, SIGLA FROM TEMP_UBICACIONES GROUP BY GRUPO, SIGLA; 
 
+
+
+SELECT 
+UBIC.GRUPO,
+SUM(SALDO)
+FROM 
+FNVOF_REPORTECATALOGO(2025, 05) RCATALOGO
+RIGHT OUTER JOIN
+TEMP_UBICACIONES UBIC ON RCATALOGO.UBICACION = UBIC.CODIGO
+WHERE 
+RCATALOGO.PRODUCTO = '076645906205-1667   '
+GROUP BY
+UBIC.GRUPO
+
+SELECT GRUPO FROM TEMP_UBICACIONES GROUP BY GRUPO
+
+
+
+SELECT 
+    UBIC.GRUPO + REPLICATE(' ', 5) + CAST(SUM(SALDO) AS VARCHAR(20)) AS ConcatenatedResult
+FROM 
+    FNVOF_REPORTECATALOGO(2025, 05) RCATALOGO,
+    TEMP_UBICACIONES UBIC
+WHERE 
+    RCATALOGO.PRODUCTO = '076645906205-1667   ' AND
+    RCATALOGO.UBICACION = UBIC.CODIGO
+GROUP BY
+    UBIC.GRUPO
+	
+
+GO
+
+*/
+
+CREATE PROCEDURE [dbo].[RQ_SaldoInventarioProducto]
+(
+    @fecha1 DATE,
+    @fecha2 DATE
+)
+AS
+BEGIN
+    DECLARE @columns NVARCHAR(MAX), @sql NVARCHAR(MAX);
+    DECLARE @year INT = YEAR(@fecha2);
+    DECLARE @month INT = MONTH(@fecha2);
+
+    -- Get the distinct GRUPO values and concatenate them into a string
+    SELECT @columns = STRING_AGG(QUOTENAME(GRUPO), ', ') 
+    FROM (SELECT DISTINCT GRUPO FROM TEMP_UBICACIONES) AS Groups;
+
+    -- Build the dynamic SQL query
+    SET @sql = N'
+    SELECT ' + @columns + ', TIPODCTO, NRODCTO, PRODUCTO
+    FROM 
+    (
+        SELECT 
+            Requisiciones.TIPODCTO,
+            Requisiciones.NRODCTO,
+            Requisiciones.PRODUCTO,
+            UBIC.GRUPO,
+            COALESCE(SUM(ISNULL(RCATALOGO.SALDO, 0)), 0) AS SALDO
+        FROM 
+            (SELECT DISTINCT TIPODCTO, NRODCTO, PRODUCTO 
+             FROM RQ_ConsolidadoRequisiciones(''' + CONVERT(NVARCHAR, @fecha1, 112) + ''', ''' + CONVERT(NVARCHAR, @fecha2, 112) + ''')
+            ) AS Requisiciones
+        LEFT JOIN 
+            FNVOF_REPORTECATALOGO(' + CAST(@year AS NVARCHAR(4)) + ', ' + CAST(@month AS NVARCHAR(2)) + ') RCATALOGO
+        ON 
+            Requisiciones.PRODUCTO = RCATALOGO.PRODUCTO
+        LEFT JOIN 
+            TEMP_UBICACIONES UBIC
+        ON 
+            RCATALOGO.UBICACION = UBIC.CODIGO
+        GROUP BY 
+            Requisiciones.TIPODCTO, Requisiciones.NRODCTO, Requisiciones.PRODUCTO, UBIC.GRUPO
+    ) AS SourceTable
+    PIVOT
+    (
+        MAX(SALDO)
+        FOR GRUPO IN (' + @columns + N')
+    ) AS PivotTable';
+
+    -- Execute the dynamic SQL query
+    EXEC sp_executesql @sql;
+END;
+GO
+
+
+
+/*
+	SELECT * FROM RQ_ConsolidadoRequisiciones('20240615', '20240626')  ORDER BY NRODCTO
+	EXEC dbo.RQ_SaldoInventarioProducto '20240615', '20240626'
 */
